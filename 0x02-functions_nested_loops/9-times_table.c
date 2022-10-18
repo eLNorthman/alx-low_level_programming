2fe4015c2933aaa87f2bf303df49eed11f40d8c8
eLNorthman <regungclay@gmail.com>
2022-10-18 16:55:10 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,9 +21,9 @@ prod = num * mult;
 if (prod <= 9)
 _putchar(' ');
 else
-_putchar((prod / 10)
+_putchar((prod / 10) + '0');
 
-_putchar((prod % 10) + '0')
+_putchar((prod % 10) + '0');
 }
 _putchar('\n');
 }
