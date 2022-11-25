faa909ae86396bf04563e68fa29caefefd3260cd
eLNorthman <regungclay@gmail.com>
2022-11-25 14:35:22 -0800

    a function that prints You're beat and yet, you must allow, bore my house upon my back before the main function is executed.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,16 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+
+void __attribute__((constructor)) hare(void);
 
 /**
- * myStartupFun - Apply the constructor attribute to myStartupFun() so that it
- *   is executed before main()
+ * hare - Prints a string before the
+ *        main function is executed.
  */
-void myStartupFun(void) __attribute__ ((constructor));
-
-/**
- * myStartupFun - implementation of myStartupFun
- */
-void myStartupFun(void)
+void hare(void)
 {
-printf("You're beat! and yet, you must allow,\n");
-printf("I bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,");
+printf("\nI bore my house upon my back!\n");
 }
